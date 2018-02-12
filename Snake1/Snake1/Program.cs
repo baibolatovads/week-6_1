436e310a665a530a9b3f87e99bef7479b6fcd5f1
Baibolatova Dariya <baibolatovads@gmail.com>
2018-02-13 00:24:40 +0600

    first

---
+++ Snake1/Snake1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Worm worm = new Worm();
+            Food food = new Food();
+            Wall wall = new Wall(1);
+
+            while (worm.isAlive)
+            {
+                Console.Clear();
+                worm.Draw();
+                food.Draw();
+                wall.Draw();
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        worm.Move(0, -1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        worm.Move(0, 1);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        worm.Move(-1, 0);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        worm.Move(1, 0);
+                        break;
+                    case ConsoleKey.Escape:
+                        worm.isAlive = false;
+                        break;
+                }
+
+                if (worm.CanEat(food))
+                {
+                    food = new Food();
+                }
+            }
+        }
+    }
+}
---
+++ Snake1/Snake1/Wall.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class Wall
+    {
+        public char sign = '#';
+        public List<Point> bricks = new List<Point>();
+        public Wall(int level)
+        {
+            string fname = string.Format(@"C:\Users\User_PC\Documents\PP2\Student\week 5\Snake\level{0}.txt", level);
+            using (FileStream fs = new FileStream(fname, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    int colNumber = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        for (int rowNumber = 0; rowNumber < line.Length; ++rowNumber)
+                        {
+                            if (line[rowNumber] == '#')
+                            {
+                                bricks.Add(new Point(rowNumber, colNumber));
+                            }
+                        }
+
+                        colNumber++;
+                    }
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < bricks.Count; ++i)
+            {
+                Console.SetCursorPosition(bricks[i].x, bricks[i].y);
+                Console.Write(sign);
+            }
+        }
+    }
+}
